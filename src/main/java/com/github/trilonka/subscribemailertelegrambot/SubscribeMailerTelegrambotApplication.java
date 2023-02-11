a23311c51509d5a7ab8db36921e510034adff34c
trilonka <trilonka@mail.ru>
2023-02-11 18:41:03 +0300

    JRTB-0: added SpringBoot skeleton to the project.

---
+++ src/main/java/com/github/trilonka/subscribemailertelegrambot/SubscribeMailerTelegrambotApplication.java
@@ -0,0 +1,13 @@
+package com.github.trilonka.subscribemailertelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscribeMailerTelegrambotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscribeMailerTelegrambotApplication.class, args);
+	}
+
+}
